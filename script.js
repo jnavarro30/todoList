b4432ffe691f68db9f1e2713a1f94b275567891b
jnavarro30 <jnavarro30@live.com>
2019-09-29 00:13:05 -0700

    Third commmit. Desktop version complete.

--- script.js
+++ script.js
@@ -1,152 +1,108 @@
-//Setup Variables
-const todoApp = document.querySelector(".todo__app");
-const todoInputEl = document.querySelector(".todo__input");
-const todoListEl = document.querySelector(".todo__list");
 
 
 
-/******************** ITEMS *********************/
 
-//Create & Add Items
+/******************** ITEMS & Details*********************/
 
 (function() {
 
-    //Create Item
+    //Setup Variables
+    const todoApp = document.querySelector(".todo__app");
+    const todoInputEl = document.querySelector(".todo__input");
+    const todoListEl = document.querySelector(".todo__list");
+    
+    
+    //Edit Item/Details & Max Character Length
 
-    let createItem = text => {
+    let textObj = {
+        editText: (event) => {
+            if(event.target.classList.contains("text")) {
+                let text = event.target;
+                text.setAttribute("contenteditable", true);
 
-        let newItem = document.createElement("li");
-        newItem.setAttribute("class", "todo__item");
-        newItem.innerHTML = `
-        <p class="todo__entry">${text}</p>
-        <span class="todo__delete">X</span>
-        <span class="todo__max"> Max Characters 80</span>
-        <p class="todo__details" >.....</p>
-        `;
-        return newItem;
+                text.addEventListener("keypress",(event) => {
+                    if(event.keyCode == 13) {
+                        text.removeAttribute("contenteditable");
+                    }
+                });
+            }
+        },
+        maxLength: (event) => {
+            if(event.target.classList.contains("text")) {
+                let text = event.target;
+                
+                if(text.textContent.length > 50) {
+                    text.textContent = text.textContent.slice(0,50);
+                    text.removeAttribute("contenteditable");
+                    let elem =text.parentElement.parentElement.lastElementChild;
+                    elem.classList.add("max");
+                    elem.classList.remove("delete__icon");
+                    elem.textContent = "MAX!";
+
+                    setTimeout(() => {
+                        elem.classList.add("delete__icon");
+                    elem.classList.remove("max");
+                    elem.textContent = "X";
+                    }, 2000);
+                }
+            }
+            
+        }
     };
 
+    todoListEl.addEventListener("keypress", textObj.maxLength);
+    todoListEl.addEventListener("dblclick", textObj.editText);
 
-    //Add Item
+    //Item Settings
 
-    todoInputEl.addEventListener("keypress", (event) => {
-
-        if(event.keyCode == 13 && todoInputEl.value) {
-            let newItem = createItem(todoInputEl.value);
-            console.log(newItem);
-            todoListEl.insertBefore(newItem, todoListEl.firstElementChild);
-            todoInputEl.value = "";
+    let itemsObj = {
+        createItem: (text) => {
+            let newItem = document.createElement("li");
+            newItem.setAttribute("class", "todo__item");
+            newItem.innerHTML = `
+            <p class="todo__entry box">
+            <span class="text__entry text">${text}</span>
+            </p>
+            <p class="todo__details box">
+            <span class="text__details text">...</span>
+            </p>
+            <span class="todo__delete delete__icon"><span class="todo__max icon">Ma</span>X</span>`;
+            return newItem;
+        },
+        addItem: (event) => {
+            if(event.keyCode == 13 && todoInputEl.value) {
+                let newItem = itemsObj.createItem(todoInputEl.value);
+                todoListEl.insertBefore(newItem, todoListEl.children[0]);
+                todoInputEl.value = "";
+            }
+        },
+        deleteItem: (event) => {
+            if(event.target.classList.contains("delete__icon")) {
+                let del = event.target;
+                del.parentElement.remove();
+            }
         }
-    });
-})();
+    };
 
-//Edit Items
+    todoInputEl.addEventListener("keypress", itemsObj.addItem);
+    todoListEl.addEventListener("click", itemsObj.deleteItem);
 
-(function(){
+    //Detail Settings
 
-    //Add Contenteditable & Max Character Limit
-
-    todoListEl.addEventListener("dblclick", (event) => {
-
-        if(event.target.classList.contains("todo__entry")) {
-            let todoEntry = event.target;
-            todoEntry.setAttribute("contenteditable", true);
-
-            let timer = setInterval(() => {
-                if(todoEntry.textContent.length > 80) {
-                    todoEntry.removeAttribute("contenteditable");
-                    todoEntry.textContent = todoEntry.textContent.slice(0,80);
-                    todoEntry.nextElementSibling.nextElementSibling.style.opacity = 1;
-                }
-                if(todoEntry.textContent.length <= 80 && todoEntry.hasAttribute("contenteditable")) {
-                 todoEntry.nextElementSibling.nextElementSibling.style.opacity = 0;
-                }
-            }, 0);
-
-
-            //Remove Contenteditable
-
-            todoListEl.addEventListener("click", (event) => {
-
-                if(!event.target.classList.contains("todo__entry")) {
-                    todoEntry.removeAttribute("contenteditable");
-
-                    if(!todoEntry.isContentEditable) {
-                        clearTimeout(timer);
-                    }
-                }
-            });
-
-            todoEntry.addEventListener("keypress", (event) => {
-
-                if(event.keyCode == 13 && todoEntry.isContentEditable) {
-                    todoEntry.removeAttribute("contenteditable");
-                }
-            });
+    let detailsObj = {
+        toggleDetails: (event) => {
+            if(event.target.classList.contains("todo__entry")) {
+                let entry = event.target;
+                entry.nextElementSibling.classList.toggle("show");
+            }else if(event.target.classList.contains("text__entry")) {
+                let text = event.target;
+                text.parentElement.nextElementSibling.classList.toggle("show");
+            }
         }
-    });
+    };
 
-
-    //Delete Items
-
-    todoListEl.addEventListener("click", (event) => {
-
-        if(event.target.classList.contains("todo__delete")) {
-            let todoDelete = event.target;
-            todoDelete.parentElement.remove();
-        }
-    });
-})();
-
-
-/******************** Details *********************/
-
-
-(function(){
-
-    //Add Contenteditable & Max Character Limit
-
-    todoListEl.addEventListener("dblclick", (event) => {
-
-        if(event.target.classList.contains("todo__details")) {
-            let todoDetails= event.target;
-            todoDetails.setAttribute("contenteditable", true);
-
-            let timer = setInterval(() => {
-                if(todoDetails.textContent.length > 80) {
-                    todoDetails.removeAttribute("contenteditable");
-                    todoDetails.textContent = todoDetails.textContent.slice(0,80);
-                    todoDetails.previousElementSibling.style.opacity = 1;
-                    todoDetails.previousElementSibling.style.marginTop = 15 + "px";
-                }
-                if(todoDetails.textContent.length <= 80 && todoDetails.hasAttribute("contenteditable")) {
-                    todoDetails.previousElementSibling.style.opacity = 0;
-                    todoDetails.previousElementSibling.style.marginTop = 0 + "px";
-                }
-            }, 0);
-
-
-            //Remove Contenteditable
-
-            todoListEl.addEventListener("click", (event) => {
-
-                if(!event.target.classList.contains("todo__details")) {
-                    todoDetails.removeAttribute("contenteditable");
-
-                    if(!todoDetails.isContentEditable) {
-                        clearTimeout(timer);
-                    }
-                }
-            });
-
-            todoDetails.addEventListener("keypress", (event) => {
-
-                if(event.keyCode == 13 && todoDetails.isContentEditable) {
-                    todoDetails.removeAttribute("contenteditable");
-                }
-            });
-        }
-    });
+    todoListEl.addEventListener("click",  detailsObj.toggleDetails);
+    
 })();
 
 
